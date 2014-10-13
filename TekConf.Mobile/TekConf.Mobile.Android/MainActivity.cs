dcb6f4825423a69e93f775723af9e923ffcc1c03
Rob Gibbens <RobGibbens@gmail.com>
2014-10-13 16:52:54 -0400

    Lots of changes

---
+++ TekConf.Mobile/TekConf.Mobile.Android/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+
+namespace TekConf.Mobile.Droid
+{
+	[Activity (Label = "TekConf", MainLauncher = true, Icon="@drawable/Icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : AndroidActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			var bootstrapper = new Bootstrapper ();
+			bootstrapper.Automapper ();
+
+			IoC.Initialize ();
+			Xamarin.Insights.Initialize (App.InsightsKey, this);
+
+			Xamarin.Forms.Forms.Init (this, bundle);
+
+			SetPage (App.GetMainPage ());
+		}
+	}
+}
---
+++ TekConf.Mobile/TekConf.Mobile/Pages/Styles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TekConf.Mobile
+{
+	public class Styles
+	{
+//		public static readonly Font LabelFont =
+//			Font.SystemFontOfSize(Device.OnPlatform(35, 40, 50), FontAttributes.Bold);
+
+		public static readonly Font LabelFont = Device.OnPlatform (
+			                                      Font.OfSize ("OpenSans-Light", 16),
+			                                      Font.SystemFontOfSize (NamedSize.Medium),
+			                                      Font.SystemFontOfSize (NamedSize.Large)
+		                                      );
+	}
+}
